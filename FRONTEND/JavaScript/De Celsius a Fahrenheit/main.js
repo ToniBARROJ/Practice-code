ffa730bf1dda33a59840becefc5153e26d9ee4a0
ToniBARROJ <toonibrojo15@gmail.com>
2025-08-06 20:31:45 +0200

    Agregar ejercicio De Celsius a Fahrenheit

---
+++ FRONTEND/JavaScript/De Celsius a Fahrenheit/main.js
@@ -0,0 +1,17 @@
+// Formula de conversión: Fahrenheit = (Celsius * 9/5) + 32
+
+/*
+- ✔ Un campo de entrada para introducir la temperatura.
+
+- ✔Un selector para elegir entre convertir de Celsius a Fahrenheit o de Fahrenheit a Celsius.
+
+- ✔ Un botón para ejecutar la conversión.
+
+- Un área de resultado que muestre la temperatura convertida.
+
+Extras opcionales (si quieres practicar más):
+
+- Validar que el usuario introduzca un número.
+
+- Cambiar el color del resultado según la temperatura (frío, templado, caliente).
+*/ 

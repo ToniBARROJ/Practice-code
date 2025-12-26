e35a53027bcd8698f0ed4011d0b1e924e0623864
ToniBARROJ <toonibrojo15@gmail.com>
2025-12-26 17:00:14 +0100

    add explanation to the code for future comprehension

--- FRONTEND/JavaScript/Cambio de moneda/Backend/server.js
+++ FRONTEND/JavaScript/Cambio de moneda/Backend/server.js
@@ -1,16 +1,27 @@
+// Constantes para usar funciones de las dependencias
 const express = require('express');
 const path = require('path');
+
+// Constante para llamar a la función exportada del archivo "api.js".
 const { getConversion } = require('./api');
+
+// Constante para crear una instancia de una aplicación Express.
 const app = express();
+
+// Constante para determinar un número de puerto.
 const PORT = 3000;
 
+const timer = 3600 * 1000;
+
+// Petición express para poder servir los archivos del directorio "Frontend"
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
+// Petición GET a la ruta "/" que sirve el archivo "index.html" del directorio "Frontend" como ruta principal cuando se inicia el servidor.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../Frontend/index.html'));
 });
 
-// Currency conversion endpoint
+// Petición GET a la ruta "/convert" que extrae los datos del frontend para después llamar a la función getConversion que hace el fetch a la API externa y devuelve un resultado.
 app.get('/convert', async (req, res) => {
   const { from, to, amount } = req.query;
 
@@ -41,4 +52,4 @@ app.listen(PORT, () => {
 
 setInterval(() => {
   console.log('Server is still running');
-}, 300000);
+}, timer);

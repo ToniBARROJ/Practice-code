8e4c443cd50c2e391a35b1f6bd199d3ed7d31aa1
ToniBARROJ <toonibrojo15@gmail.com>
2025-12-26 16:59:34 +0100

    clean code, the api key is not used

--- FRONTEND/JavaScript/Cambio de moneda/Backend/api.js
+++ FRONTEND/JavaScript/Cambio de moneda/Backend/api.js
@@ -2,7 +2,7 @@ const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 async function getConversion(from, to, amount) {
-  const url = `https://api.exchangerate.host/convert?access_key=72e845380d2cfb60ccc3ea6e9302a292&from=${from}&to=${to}&amount=${amount}`;
+  const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
 
   const response = await fetch(url);
 

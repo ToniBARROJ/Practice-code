71efeb58fd3644fdc1a649d4d959461e054f1cd3
ToniBARROJ <toonibrojo15@gmail.com>
2025-12-26 15:43:59 +0100

    add api file to separate work

---
+++ FRONTEND/JavaScript/Cambio de moneda/Backend/api.js
@@ -0,0 +1,20 @@
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+async function getConversion(from, to, amount) {
+  const url = `https://api.exchangerate.host/convert?access_key=72e845380d2cfb60ccc3ea6e9302a292&from=${from}&to=${to}&amount=${amount}`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Error al obtener datos de la API');
+  }
+  const data = await response.json();
+
+  if (!data.result) {
+    throw new Error('Respuesta inválida de la API');
+  }
+  return data.result;
+}
+
+module.exports = { getConversion };

73245ba60c2efefe4227ac3d71092442d8a2f503
ToniBARROJ <toonibrojo15@gmail.com>
2026-01-12 21:18:49 +0100

    fix nod-fetch import

--- FRONTEND/JavaScript/Cambio de moneda/Backend/api.js
+++ FRONTEND/JavaScript/Cambio de moneda/Backend/api.js
@@ -1,20 +1,23 @@
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+const API_KEY = '3b6219f6b9392c9563ff4f47';
+
 async function getConversion(from, to, amount) {
-  const url = `https://api.massive.com/v1/conversion/${from}/${to}?amount=${amount}&precision=2&apiKey=6P6Twlq87yQAfpL4SDC80kcK3s7VNP0z`;
+  const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${from}/${to}/${amount}`;
 
   const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error('Error al obtener datos de la API');
+    throw new Error('Error en la API externa');
   }
-  const data = await response.json();
 
-  if (!data.result) {
+  const data = await response.json();
+  if (data.result !== 'success') {
     throw new Error('Respuesta inválida de la API');
   }
-  return data.result;
+
+  return data.conversion_result;
 }
 
 module.exports = { getConversion };
